8ac0eb5c0ba7dff43c4e121c9dfa31c88760ab4f
Maxim Batourine <35441642+batourin@users.noreply.github.com>
2022-10-19 16:28:08 -0400

    remove old angular ui

---
+++ GalleryUI/src/models/display.model.ts
@@ -0,0 +1,13 @@
+export const enum ProjectorState {
+    None = 0,
+    WarmingUp = 1,
+    CollingDown = 2,
+  }
+
+  export interface Display {
+  id: string;
+  name: string;
+  power: boolean;
+  state: ProjectorState;
+  lamp: number;
+}
--- GalleryUI/src/stores/crestron.ts
+++ GalleryUI/src/stores/crestron.ts
@@ -1,8 +1,10 @@
 import { ref, computed } from "vue";
+import type { Display } from "@/models/display.model";
 import { defineStore } from "pinia";
 
 export const useCrestronStore = defineStore("crestron", {
   state: () => ({
+    displays: Display[]
   }),
   getters: {
 
